4752b67d676892627675292d8a31fc8dd95cdab1
wuPiotrek <wupiotrek@gmail.com>
2024-12-05 09:35:57 +0100

    changed publications array to array list with dynamic size doubling added delete publication option

--- src/pl/javastart/library/app/LibraryControl.java
+++ src/pl/javastart/library/app/LibraryControl.java
@@ -53,6 +53,12 @@ class LibraryControl {
                 case PRINT_MAGAZINES:
                     printMagazines();
                     break;
+                case DELETE_BOOK:
+                    deleteBook();
+                    break;
+                case DELETE_MAGAZINE:
+                    deleteMagazine();
+                    break;
                 case EXIT:
                     exit();
                     break;
@@ -97,6 +103,18 @@ class LibraryControl {
         }
     }
 
+    private void deleteBook() {
+        try {
+            Book book = dataReader.readAndCreateBook();
+            if (library.removePublication(book)){
+                printer.printLine("Usunięto książkę");
+            }
+            else printer.printLine("Brak wskazanej książki");
+        } catch (InputMismatchException e) {
+            printer.printLine("Nie udało się usunąć książki, niepoprawne dane");
+        }
+    }
+
     private void printBooks() {
         Publication[] publications = library.getPublications();
         printer.printBooks(publications);
@@ -110,6 +128,18 @@ class LibraryControl {
             printer.printLine("Nie udało się utworzyć magazynu, niepoprawne dane");
         } catch (ArrayIndexOutOfBoundsException e) {
             printer.printLine("Osiągnięto limit pojemności, nie można dodać kolejnego magazynu");
+        }
+    }
+
+    private void deleteMagazine() {
+        try {
+            Magazine magazine = dataReader.readAndCreateMagazine();
+            if (library.removePublication(magazine)){
+                printer.printLine("Usunięto magazyn");
+            }
+            else printer.printLine("Brak wskazanego magazynu");
+        } catch (InputMismatchException e) {
+            printer.printLine("Nie udało się usunąć magazynu, niepoprawne dane");
         }
     }
 
@@ -134,7 +164,9 @@ class LibraryControl {
         ADD_BOOK(1, "Dodanie książki"),
         ADD_MAGAZINE(2,"Dodanie magazynu/gazety"),
         PRINT_BOOKS(3, "Wyświetlenie dostępnych książek"),
-        PRINT_MAGAZINES(4, "Wyświetlenie dostępnych magazynów/gazet");
+        PRINT_MAGAZINES(4, "Wyświetlenie dostępnych magazynów/gazet"),
+        DELETE_BOOK(5, "Usuń książkę"),
+        DELETE_MAGAZINE(6, "Usuń magazyn");
 
         private int value;
         private String description;
--- src/pl/javastart/library/model/Library.java
+++ src/pl/javastart/library/model/Library.java
@@ -1,12 +1,13 @@
 package pl.javastart.library.model;
 
 import java.io.Serializable;
+import java.util.Arrays;
 
 public class Library implements Serializable {
 
-    private static final int MAX_PUBLICATONS = 2000;
+    private static final int INITIAL_CAPACITY = 1;
     private int publicationsNumber;
-    private Publication[] publications = new Publication[MAX_PUBLICATONS];
+    private Publication[] publications = new Publication[INITIAL_CAPACITY];
 
     public Publication[] getPublications() {
         Publication[] result = new Publication[publicationsNumber];
@@ -17,10 +18,27 @@ public class Library implements Serializable {
     }
 
     public void addPublication(Publication publication) {
-        if (publicationsNumber >= MAX_PUBLICATONS) {
-            throw new ArrayIndexOutOfBoundsException("Max publications exceeded " + MAX_PUBLICATONS);
+        if (publicationsNumber >= publications.length) {
+            publications = Arrays.copyOf(publications, publications.length * 2);
         }
         publications[publicationsNumber] = publication;
         publicationsNumber++;
+    }
+
+    public boolean removePublication(Publication pub) {
+        final int notFound = -1;
+        int found = notFound;
+        int i = 0;
+        while (i < publicationsNumber && found == notFound) {
+            if (pub.equals(publications[i]))
+                found = i;
+            else i++;
+        }
+        if (found != notFound) {
+            System.arraycopy(publications, found + 1, publications, found, publications.length - found - 1 );
+            publicationsNumber--;
+            publications[publicationsNumber] = null;
+        }
+        return found != notFound;
     }
 }
