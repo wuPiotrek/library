015d899baaf344fdb886ab34d957e1a308ddb291
wuPiotrek <wupiotrek@gmail.com>
2024-12-13 11:43:40 +0100

    code refactored using method references which replaces lambda expressions

--- src/pl/javastart/library/app/LibraryControl.java
+++ src/pl/javastart/library/app/LibraryControl.java
@@ -5,11 +5,9 @@ import pl.javastart.library.io.ConsolePrinter;
 import pl.javastart.library.io.DataReader;
 import pl.javastart.library.io.file.FileManager;
 import pl.javastart.library.io.file.FileManagerBuilder;
-import pl.javastart.library.model.Book;
-import pl.javastart.library.model.Library;
-import pl.javastart.library.model.LibraryUser;
-import pl.javastart.library.model.Magazine;
+import pl.javastart.library.model.*;
 
+import java.util.Comparator;
 import java.util.InputMismatchException;
 
 class LibraryControl {
@@ -73,7 +71,7 @@ class LibraryControl {
 
     private void printUsers() {
         printer.printUsers(library.getSortedUsers(
-                (p1, p2) -> p1.getLastName().compareToIgnoreCase(p2.getLastName())
+                Comparator.comparing(User::getLastName, String.CASE_INSENSITIVE_ORDER)
         ));
     }
 
@@ -135,7 +133,7 @@ class LibraryControl {
 
     private void printBooks() {
         printer.printBooks(library.getSortedPublications(
-                (p1, p2) -> p1.getTitle().compareToIgnoreCase(p2.getTitle())
+                (Comparator.comparing(Publication::getTitle, String.CASE_INSENSITIVE_ORDER))
         ));
     }
 
@@ -163,7 +161,7 @@ class LibraryControl {
 
     private void printMagazines() {
         printer.printMagazines(library.getSortedPublications(
-                (p1, p2) -> p1.getTitle().compareToIgnoreCase(p2.getTitle())
+                (Comparator.comparing(Publication::getTitle, String.CASE_INSENSITIVE_ORDER))
         ));
     }
 
--- src/pl/javastart/library/model/User.java
+++ src/pl/javastart/library/model/User.java
@@ -3,7 +3,7 @@ package pl.javastart.library.model;
 import java.io.Serializable;
 import java.util.Objects;
 
-abstract class User implements Serializable, CsvConvertible {
+public abstract class User implements Serializable, CsvConvertible {
     private String firstName;
     private String lastName;
     private String pesel;
